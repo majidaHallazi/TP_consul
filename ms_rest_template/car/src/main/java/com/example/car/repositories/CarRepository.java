e349b29068c838e1d096289e6bf6858b645b0dff
Majida Hallazi <dmaj3900@gmail.com>
2025-01-12 12:24:35 +0100

    tp

--- ms_rest_template/car/src/main/java/com/example/car/repositories/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.car.repositories;
-
-import com.example.car.entities.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car,Long> { }
